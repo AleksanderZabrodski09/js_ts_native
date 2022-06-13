651ef06156d84bce367d642ee0e4d4605b895b0c
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-06-13 23:33:05 +0200

    create: add lesson-1(tests)

--- src/01-hello-tests/01.ts
+++ src/01-hello-tests/01.ts
@@ -1,4 +1,18 @@
+export function sum(a: number, b: number) {
+  return a + b;
+}
 
-export function splitIntoWords(sentense: string) {
-    return ["hello", "my", "friends"]
+export function mult(a: number, b: number) {
+  return a * b;
+}
+
+
+export function splitIntoWords(sentence: string) {
+  const words = sentence.toLowerCase().split('')
+
+  return words.filter(w => w !== '' && w !== '-')
+    .map(w => w
+      .replace('!', '')
+      .replace('.', '')
+      .replace(',', ''));
 }
--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import {splitIntoWords} from "./01-hello-tests/01";
 
 
 const sentense = "Hello my friends!";
-const result = splitIntoWords("sentense")
+const result = splitIntoWords(sentense)
 console.log(result)
 
 const root = ReactDOM.createRoot(
